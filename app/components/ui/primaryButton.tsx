e34e267be84303620d49c5ee753e3d06ec173159
bennyalva <bennytescha@gmail.com>
2026-05-26 13:51:47 -0600

    se mejoran colores de los themas en el componente prymaryButton.tsx

--- app/components/ui/primaryButton.tsx
+++ app/components/ui/primaryButton.tsx
@@ -9,7 +9,7 @@ export const PrimaryButton = ({ onClick, children, disabled }: PrimaryButtonProp
             className={`w-full py-2 rounded-xl font-semibold text-lg transition-all duration-200 text-center
         ${disabled
                     ? 'bg-disabled-bg text-disabled-text cursor-not-allowed'
-                    : 'bg-active-bg text-active-text hover:brightness-90 active:scale-[0.99] cursor-pointer shadow-md'
+                    : 'bg-active-bg text-active-text hover:bg-active-bg-hover active:scale-[0.99] cursor-pointer shadow-md'
                 }`}
         >
             {children}
