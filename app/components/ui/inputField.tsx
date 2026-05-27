8d7184ed9e5f8ee9e01c0775e341876cf36120ba
bennyalva <bennytescha@gmail.com>
2026-05-26 22:22:19 -0600

    se extraen los svg de inputField.tsx para que quede mas estructurado el componente

--- app/components/ui/inputField.tsx
+++ app/components/ui/inputField.tsx
@@ -2,6 +2,7 @@
 
 import { InputFieldProps } from "../interfacesComponents";
 import { useSpeechRecognition } from "@/app/hooks/useSpeechRecognition/useSpeechRecognition";
+import SpeechIcon from "@/app/components/shared/icons/speechIcon";
 
 export const InputField = ({
   label,
@@ -45,37 +46,7 @@ export const InputField = ({
               }`}
             title="Dictar por voz"
           >
-            {isListening ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M5.25 7.5A2.25 2.25 0 017.5 5.25h9a2.25 2.25 0 012.25 2.25v9a2.25 2.25 0 01-2.25 2.25h-9a2.25 2.25 0 01-2.25-2.25v-9z"
-                />
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 18.75a6 6 0 006-6v-1.5m-6 7.5a6 6 0 01-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 01-3-3V4.5a3 3 0 116 0v8.25a3 3 0 01-3 3z"
-                />
-              </svg>
-            )}
+            <SpeechIcon listening={isListening} />
           </button>
         )}
       </div>
