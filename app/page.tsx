9a8f067a9e5f2ca24ff6d93559e8bb6e9653c1f7
bennyalva <bennytescha@gmail.com>
2026-05-29 10:22:24 -0600

    se agregan colores de tema a las paginas home, login y admin

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="min-h-screen w-full flex flex-col justify-between items-center
-                    bg-bg-app px-6 py-4 transition-colors duration-300">
+                    bg-app text-primary px-6 py-4 transition-colors duration-300">
       home
     </div>
   );
